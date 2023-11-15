f5d669615b3a5214da46f197d67a1260d3acc39e
Alec Hampton <admin@thewebfromscratch.com>
2023-11-15 16:30:16 -0700

    Green

--- React/appointments-example-app/src/Appointment.js
+++ React/appointments-example-app/src/Appointment.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+const getTimeStringFrom = (startsAt) => {
+  const [h, m] = new Date(startsAt).toTimeString().split(":");
+  return `${h}:${m}`;
+};
+
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
 
 export const AppointmentsDayView = ({ appointments }) => (
   <div id="appointmentsDayView">
     <ol>
       {appointments.map((appointment) => (
-        <li key={appointment.startsAt} />
+        <li key={appointment.startsAt}>
+          {getTimeStringFrom(appointment.startsAt)}
+        </li>
       ))}
     </ol>
   </div>
